1660692caf4302fa7dc879f342780f071f1e8955
mostafabcb <37628401+mostafabcb@users.noreply.github.com>
2025-03-15 19:15:57 +0600

    Update phone-spec.js

--- phone-spec.js
+++ phone-spec.js
@@ -47,7 +47,7 @@ const specIcons = {
 document.addEventListener('DOMContentLoaded', function() {
     const styles = {
         phoneSpecs: {
-            lineHeight: '1.5',
+            lineHeight: '1.1',
             maxWidth: '600px',
             margin: '0 auto',
             padding: '15px',
